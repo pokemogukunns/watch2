3f4b3203ee84482051a786c43be835bdaf1654dd
pokemogukunns <nattyannkunndesu@icloud.com>
2024-10-15 10:53:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cache import cache
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://inv.nadeko.net/",r"https://invidious.jing.rocks/",r"https://invidious.flokinet.to/"]
+apis = [r"https://inv.nadeko.net/",r"https://invidious.flokinet.to/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
